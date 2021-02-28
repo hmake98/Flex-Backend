9774d7dc2bcaa10726ac585048d3c5b003415cbb
Harsh Makwana <harsh.m@simformsolutions.com>
2021-02-28 20:38:26 +0530

    minor fixes

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,14 +1,4 @@
-import { Request, Response, NextFunction } from 'express'
-import { User } from '../models/User'
 import { Responses } from '../utils/Response'
-import { USER } from './../utils/messages';
-import {
-    checkPassword,
-    generatePassword,
-    generate_tokens,
-    uploadToS3Bucket
-} from './../utils/helper';
-import logger from '../services/logger.service'
 
 export class PostController extends Responses {
 
